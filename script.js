546657446b6d403d130fddda76781d967f244304
Flovedev <flove.dev@gmail.com>
2023-01-16 16:58:31 +0000

    clean code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const getPink = () => {
-    fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=pink-floyd', {
+const getContent = (search) => {
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`, {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'c4ae4acbf2msh44233229ff4ffffp145753jsne924cf812d8a',
@@ -8,63 +8,7 @@ const getPink = () => {
     }).then((rawBand) => {
         return rawBand.json()
     }).then((jsonBand) => {
-        let container = document.getElementById('pink-container')
-        for (let index = 0; index < jsonBand.data.length; index++) {
-            const element = jsonBand.data[index];
-            container.innerHTML += `
-            <div class="card m-1" style="width: 18rem;">
-                <img src="${element.album.cover_xl}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${element.title}</h5>
-                    <p class="card-text">${element.album.title}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Duration: ${Math.round((element.duration / 60 + Number.EPSILON) * 100) / 100}</li>
-                </ul>
-            </div>`
-        }
-    })
-}
-
-const getDaft = () => {
-    fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=daft-punk', {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'c4ae4acbf2msh44233229ff4ffffp145753jsne924cf812d8a',
-            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-        }
-    }).then((rawBand) => {
-        return rawBand.json()
-    }).then((jsonBand) => {
-        let container = document.getElementById('daft-container')
-        for (let index = 0; index < jsonBand.data.length; index++) {
-            const element = jsonBand.data[index];
-            container.innerHTML += `
-            <div class="card m-1" style="width: 18rem;">
-                <img src="${element.album.cover_xl}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${element.title}</h5>
-                    <p class="card-text">${element.album.title}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Duration: ${Math.round((element.duration / 60 + Number.EPSILON) * 100) / 100}</li>
-                </ul>
-            </div>`
-        }
-    })
-}
-
-const getMetallica = () => {
-    fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica', {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'c4ae4acbf2msh44233229ff4ffffp145753jsne924cf812d8a',
-            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-        }
-    }).then((rawBand) => {
-        return rawBand.json()
-    }).then((jsonBand) => {
-        let container = document.getElementById('metallica-container')
+        let container = document.getElementById(`${search}-container`)
         for (let index = 0; index < jsonBand.data.length; index++) {
             const element = jsonBand.data[index];
             container.innerHTML += `
@@ -106,7 +50,7 @@ const songList = () => {
 }
 
 window.onload = function () {
-    getPink();
-    getDaft();
-    getMetallica();
+    getContent("pink-floyd");
+    getContent("daft-punk");
+    getContent("metallica");
 };
